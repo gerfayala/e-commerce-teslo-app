e634b2b5466619ce7e2e6337bdea3f87784f0c3d
German <germanfayala2419@gmail.com>
2025-02-05 17:28:56 -0400

    feat: Implement login logout functionality

--- core/auth/api/productsApi.ts
+++ core/auth/api/productsApi.ts
@@ -1,3 +1,5 @@
+import {SecureStorageAdapter} from '@/helpers/adapters/secure-storage.adapter';
+import {A} from '@expo/html-elements';
 import axios from 'axios';
 import {Platform} from 'react-native';
 
@@ -6,6 +8,8 @@ const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 const apiUrlIos = process.env.EXPO_PUBLIC_API_URL_IOS;
 
 const apiUrlAndroid = process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+const apiWeb = process.env.EXPO_PUBLIC_API_URL_WEB;
 
 const STAGE = process.env.EXPO_PUBLIC_STAGE || 'dev';
 
@@ -16,12 +20,24 @@ export const API_URL =
     ? apiUrlIos
     : Platform.OS === 'android'
     ? apiUrlAndroid
+    : Platform.OS === 'web'
+    ? apiWeb
     : apiUrl;
-
-console.log({STAGE, [Platform.OS]: API_URL});
 
 const productsApi = axios.create({
   baseURL: API_URL
+});
+
+productsApi.interceptors.request.use(async (config) => {
+  //Verify if we have token on secure storage
+
+  const token = await SecureStorageAdapter.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export {productsApi};
